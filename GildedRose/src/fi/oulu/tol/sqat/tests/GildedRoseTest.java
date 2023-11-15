485d24735f6dd05807a0cf2efdf24d3db62fddc7
Aleksi Santala <asantala22@student.oulu.fi>
2023-11-15 22:01:36 +0200

    commit 1

--- GildedRose/src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ GildedRose/src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -14,7 +14,7 @@ public class GildedRoseTest {
 	@Test
 	public void testTheTruth() {
 		assertTrue(true);
-	}
+	} 
 	@Test
 	public void exampleTest() {
 		//create an inn, add an item, and simulate one day
@@ -28,5 +28,106 @@ public class GildedRoseTest {
 		
 		//assert quality has decreased by one
 		assertEquals("Failed quality for Dexterity Vest", 19, quality);
+	}
+	
+	@Test
+	public void agedBrieTest() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Aged Brie", 10, 20));
+		inn.oneDay();
+		assertEquals("Failed quality for Aged Brie", 21, inn.getItems().get(0).getQuality());
+		
+		inn.getItems().get(0).setQuality(50);
+		inn.oneDay();
+		assertEquals("Failed quality for Aged Brie", 50, inn.getItems().get(0).getQuality());
+		
+		inn.getItems().get(0).setSellIn(0);
+		inn.oneDay();
+		assertEquals("Failed quality for Aged Brie", 50, inn.getItems().get(0).getQuality());
+		
+		inn.getItems().get(0).setQuality(0);
+		inn.oneDay();
+		assertEquals("Failed quality for Aged Brie", 2, inn.getItems().get(0).getQuality());
+	}
+	
+	@Test
+	public void sulfurasTest() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Sulfuras, Hand of Ragnaros", 10, 80));
+		inn.oneDay();
+		assertEquals("Failed quality for Sulfuras, Hand of Ragnaros", 80, inn.getItems().get(0).getQuality());
+		
+		inn.getItems().get(0).setSellIn(-1);
+		inn.oneDay();
+		assertEquals("Failed quality for Sulfuras, Hand of Ragnaros", 80, inn.getItems().get(0).getQuality());
+		
+	}
+	
+	@Test
+	public void backstageTest() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Backstage passes to a TAFKAL80ETC concert", 11, 49));
+		inn.oneDay();
+		assertEquals("Failed quality for Backstage passes", 50, inn.getItems().get(0).getQuality());
+		
+		inn.oneDay();
+		assertEquals("Failed quality for Backstage passes", 50, inn.getItems().get(0).getQuality());
+		
+		inn.getItems().get(0).setQuality(0);
+		inn.oneDay();
+		assertEquals("Failed quality for Backstage passes", 2, inn.getItems().get(0).getQuality());
+		
+		inn.getItems().get(0).setSellIn(5);
+		inn.getItems().get(0).setQuality(49);
+		inn.oneDay();
+		assertEquals("Failed quality for Backstage passes", 50, inn.getItems().get(0).getQuality());
+		
+		inn.oneDay();
+		assertEquals("Failed quality for Backstage passes", 50, inn.getItems().get(0).getQuality());
+		
+		inn.getItems().get(0).setQuality(0);
+		inn.oneDay();
+		assertEquals("Failed quality for Backstage passes", 3, inn.getItems().get(0).getQuality());
+		
+		inn.getItems().get(0).setSellIn(0);
+		inn.oneDay();
+		assertEquals("Failed quality for Backstage passes", 0, inn.getItems().get(0).getQuality());
+	}
+	
+	@Test
+	public void backstageMutantTest() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20));
+		inn.oneDay();
+		assertEquals("Failed quality for Backstage passes", 21, inn.getItems().get(0).getQuality());
+		inn.getItems().get(0).setSellIn(6);
+		inn.oneDay();
+		assertEquals("Failed quality for Backstage passes", 23, inn.getItems().get(0).getQuality());
+		inn.getItems().get(0).setSellIn(1);
+		inn.oneDay();
+		assertEquals("Failed quality for Backstage passes", 26, inn.getItems().get(0).getQuality());
+		
+	}
+	
+	@Test
+	public void basicDrinkTest() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Basic drink", 0, 10));
+		inn.oneDay();
+		assertEquals("Failed quality for basic drink", 8, inn.getItems().get(0).getQuality());
+		assertEquals("Failed SellIn for basic drink", -1, inn.getItems().get(0).getSellIn());
+		
+		inn.getItems().get(0).setQuality(-1);
+		inn.oneDay();
+		assertEquals("Failed quality for basic drink", -1, inn.getItems().get(0).getQuality());
+		
+		inn.getItems().get(0).setQuality(0);
+		inn.oneDay();
+		assertEquals("Failed quality for basic drink", 0, inn.getItems().get(0).getQuality());
+	}
+	
+	@Test
+	public void mainTest() {
+		GildedRose.main(null);
 	}
 }
